b3e885a63ff8a28ed1273b2ebcd5b717c31a97e1
Weldmicheal Berhanu <michock.mit@gmail.com>
2021-09-28 18:03:27 -0500

    updates to mean

--- HomeWorks/Day02/MEANGame/meangame.js
+++ HomeWorks/Day02/MEANGame/meangame.js
@@ -5,6 +5,11 @@ const path = require("path")
 const app = express()
 app.set("port", 3000)
 
+app.use(function(req, res, next){
+    console.log(req.method, req.url)
+    next()
+})
+
 app.use(express.static(path.join(__dirname, "public")))
 
 const server = app.listen(app.get("port"), function(){
