b92a5d33ce471a9ebc62261419e1aa4d8d8a9227
Weldmicheal Berhanu <michock.mit@gmail.com>
2021-09-28 18:16:43 -0500

    changes

--- HomeWorks/Day02/MEANGame/meangame.js
+++ HomeWorks/Day02/MEANGame/meangame.js
@@ -1,5 +1,4 @@
 const express = require("express")
-
 const path = require("path")
 
 const app = express()
@@ -13,5 +12,5 @@ app.use(function(req, res, next){
 app.use(express.static(path.join(__dirname, "public")))
 
 const server = app.listen(app.get("port"), function(){
-    console.log("listening to port", server.address().port)
+    console.log("listening on port", server.address().port)
 })
