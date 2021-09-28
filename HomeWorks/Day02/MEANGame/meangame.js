6e58303290c5ef92cb4f7fd98219430771a436ca
Weldmicheal Berhanu <michock.mit@gmail.com>
2021-09-28 17:51:18 -0500

    Update meangame.js

--- HomeWorks/Day02/MEANGame/meangame.js
+++ HomeWorks/Day02/MEANGame/meangame.js
@@ -7,11 +7,6 @@ app.set("port", 3000)
 
 app.use(express.static(path.join(__dirname, "public")))
 
-// app.get("/", function(req, res) {
-//     console.log("GET received!")
-
-//     res.status(200).sendFile(path.join(__dirname, "public", "index.html"))
-// })
 
 const server = app.listen(app.get("port"), function(){
     console.log("listening to port", server.address().port)
